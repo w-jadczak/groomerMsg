715b7478633be4e47a21634a765306464c968afa
w-jadczak <w.jadczak@yahoo.com>
2024-04-04 16:44:48 +0200

    inintial commit

---
+++ src/test/java/com/wjadczak/groomerMsg/GroomerMsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjadczak.groomerMsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomerMsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
