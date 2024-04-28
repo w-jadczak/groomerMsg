b32449c23a30a6a6bd4b83a8b6b8bc890fdd6dbf
w-jadczak <w.jadczak@yahoo.com>
2024-04-28 20:03:48 +0200

    adding skech classes for security implementation

--- src/main/java/com/wjadczak/groomerMsg/config/security/SecurityConfig.java
+++ src/main/java/com/wjadczak/groomerMsg/config/security/SecurityConfig.java
@@ -1,10 +1,12 @@
-package com.wjadczak.groomerMsg.config;
+package com.wjadczak.groomerMsg.config.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
 
